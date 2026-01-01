d9c6783d0a5cf335a19600dd9dbd814bdc57298d
linhnt <nguyenthilinh2150@gmail.com>
2026-01-01 23:51:21 +0700

    linh-update-code-search

--- test-mapping/Recruitment_Candidate_Data.ts
+++ test-mapping/Recruitment_Candidate_Data.ts
@@ -2,4 +2,3 @@ import RecruitmentCandidateData from '../data/Recruitment_Candidate.json';
 import type {RecruitmentCandidateForm} from '../types/Recruitment_Candidate.types';
 
 export const candidateData: RecruitmentCandidateForm = RecruitmentCandidateData.validCandidate;
-export const requiredFieldsCandidateData: RecruitmentCandidateForm = RecruitmentCandidateData.requiredFieldsCandidate;
--- test-mapping/Recruitment_Vacancy_Data.ts
+++ test-mapping/Recruitment_Vacancy_Data.ts
@@ -2,4 +2,3 @@ import RecruitmentVacancyData from '../data/Recruitment_Vacancy.json';
 import type {RecruitmentVacancyForm} from '../types/Recruitment_Vacancy.types';
 
 export const validVacancyData: RecruitmentVacancyForm = RecruitmentVacancyData.validVacancy;
-export const requiredFieldsVacancyData: RecruitmentVacancyForm = RecruitmentVacancyData.requiredFieldsVacancy;
