86dc6f6c414e9d44096f3368bdf3e7f7d8ea3a46
hoaintk <hoaingkh@gmail.com>
2026-01-04 22:27:29 +0700

    update description for api section

--- common/API/waitAPI.ts
+++ common/API/waitAPI.ts
@@ -7,16 +7,17 @@ export default class ApiBasePage {
     this.page = page;
   }
 
+  // res: actual response
   async waitForAPIResponse(
     endpointContains: string,
     method: string,
     statusCode: number,
-    timeoutMs = 15_000
+    timeoutMs = 5000
   ): Promise<Response> {
     return await this.page.waitForResponse(
-      (res) => {
+      (res) => {   
         const urlOk = res.url().includes(endpointContains);
-        if (!urlOk) return false; 
+        if (!urlOk) return false;  // check hết toàn bộ res trả về xem có res nào có url mapping với endpoint cần check không
 
         const methodOk = res.request().method() === method;
         const statusOk = res.status() === statusCode;
